86e07214d09fb387542bc68d3eb3c580f92e578b
admin <admin@liyizhedeheipingguo.local>
2015-09-15 23:19:30 +0800

    Initial Commit

---
+++ yueFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yueFM
+//
+//  Created by admin on 15/9/15.
+//  Copyright (c) 2015年 李懿哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
