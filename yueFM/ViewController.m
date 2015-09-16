91ebd4cb3bc4e990058f1515ff2bace6e0238237
lyz <li_yizhe@qq.com>
2015-09-16 21:01:05 +0800

    能加载到数据，并显示，没有加滚动

--- yueFM/ViewController.m
+++ yueFM/ViewController.m
@@ -6,22 +6,48 @@
 //  Copyright (c) 2015年 李懿哲. All rights reserved.
 //
 
+#define PRE_ARTILE_URL @"http://yue.fm/"
+#define RANDOM_URL @"http://api.yue.fm/articles/random"
+
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(nonatomic, strong)UIWebView *webView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+           //设置webview
+           [self setWebView];
+           //加载一篇文章
+           [self loadARandomArticle];
 }
 
+-(void)setWebView{
+           self.webView = [UIWebView new];
+           self.webView.frame = self.view.bounds;
+           [self.view addSubview:self.webView];
+           
+           //增加拖拽事件
+           [self addDragGesture];
+}
+//加载随机文章
+-(void)loadARandomArticle{
+           [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[self getARandomArticleURL]]]];
+}
+//获取一个随机文章的地址
+-(NSString *)getARandomArticleURL{
+           NSURL *url = [NSURL URLWithString:RANDOM_URL];
+           NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:url] options:NSJSONReadingMutableContainers error:nil];
+           return [NSString stringWithFormat:@"%@%@", PRE_ARTILE_URL, dic[@"short_id"]];
+}
+-(void)addDragGesture{
+          
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
