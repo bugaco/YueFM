19abf4cf19ef8a59219a5f4a843d91d16ae50ff4
admin <admin@liyizhedeheipingguo.local>
2015-09-21 00:57:49 +0800

    禁用了横评模式

--- yueFM/AppDelegate.m
+++ yueFM/AppDelegate.m
@@ -41,5 +41,9 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
-
+//禁用竖屏
+-(NSUInteger)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
 @end
